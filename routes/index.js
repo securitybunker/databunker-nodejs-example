c2faae5b1546c2e7dea754c6e86e5e735f5452e0
root <root@databunker.org>
2020-12-18 15:04:36 +0000

    create a register code and login

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const MAGIC_PUBLISHABLE_KEY = process.env.MAGIC_PUBLISHABLE_KEY;
-
 // GET home page
 router.get("/", (req, res) => {
-  res.render("index", { title: "Magic Apple Store 🍎", MAGIC_PUBLISHABLE_KEY });
+  res.render("index", { title: "Magic Apple Store 🍎" });
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const MAGIC_PUBLISHABLE_KEY = process.env.MAGIC_PUBLISHABLE_KEY;
 
 const DatabunkerStore = require('@databunker/store');
 const databunker = new DatabunkerStore({
@@ -19,8 +20,11 @@ const strategy = new MagicStrategy(async function(user, done) {
   const userMetadata = await magic.users.getMetadataByIssuer(user.issuer);
   const existingUser = await databunker.users.get("email", userMetadata.email);
   if (!existingUser.data) {
-    /* Create new user if doesn't exist */
-    return signup(user, userMetadata, done);
+    // do not automatically register users here
+    // return signup(user, userMetadata, done);
+    return done(null, false, {
+      message: `User record was not found. You need to register first.`
+    });
   } else {
     /* Login user if otherwise */
     return login(user, existingUser, done);
@@ -29,9 +33,8 @@ const strategy = new MagicStrategy(async function(user, done) {
 
 passport.use(strategy);
 
-/* 3️⃣ Implement Auth Behaviors */
-
-/* Implement User Signup */
+/* No not automatically signup user, we have a register code above */
+/*
 const signup = async (user, userMetadata, done) => {
   let newUser = {
     issuer: user.issuer,
@@ -45,6 +48,7 @@ const signup = async (user, userMetadata, done) => {
   };
   return done(null, returnUser);
 };
+*/
 
 /* Implement User Login */
 const login = async (user, existingUser, done) => {
@@ -63,6 +67,58 @@ const login = async (user, existingUser, done) => {
   };
   return done(null, returnUser);
 };
+
+router.get('/register', (req, res) => {
+    let errors = [];
+    res.render('register', {errors, name:'', email: '', company: ''})
+});
+
+router.post('/register', async (req, res) => {
+    let errors = [];
+    if (!req.body.name) {
+        errors.push({ text: 'Please Enter Name' });
+    }
+    if (!req.body.email) {
+        errors.push({ text: 'Please Enter Email' });
+    }
+    if (!req.body.company) {
+        errors.push({ text: 'Please Enter Company' });
+    }
+    const existingUser = await databunker.users.get("email", req.body.email);
+    if (existingUser.data) {
+        errors.push({ text: 'Email Already Exists'} );
+    }
+    if (errors.length > 0) {
+        res.render('register', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            company: req.body.company
+        });
+	return;
+    }
+    const user = {
+        name: req.body.name,
+        email: req.body.email,
+        company: req.body.company
+    };
+    const result = await databunker.users.create(user);
+    if (result.status == "ok") {
+        res.redirect('/user/login');
+    } else {
+        errors.push({ text: result.message } );
+        res.render('register', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            company: req.body.company
+        });
+    }
+});
+
+router.get('/login', (req, res) => {
+    res.render("login", { title: "Magic Apple Store 🍎", MAGIC_PUBLISHABLE_KEY });
+});
 
 /* Attach middleware to login endpoint */
 router.post("/login", passport.authenticate("magic"), (req, res) => {
@@ -92,11 +148,11 @@ passport.deserializeUser(async (user, done) => {
 /* 5️⃣ Implement User Endpoints */
 
 /* Implement Get Data Endpoint */
-router.get("/", async (req, res) => {
+router.get("/me", async (req, res) => {
   if (req.isAuthenticated()) {
     const data = await databunker.collection("data").get("token", req.user.token);
     const j = {...req.user,
-      appleCount: data.data.appleCount
+      appleCount: ((data.data && data.data.appleCount)? data.data.appleCount : 0)
     };
     return res
       .status(200)
@@ -112,7 +168,7 @@ router.post("/buy-apple", async (req, res) => {
   if (req.isAuthenticated()) {
     const data = await databunker.collection("data").get("token", req.user.token);
     await databunker.collection("data").set("token", req.user.token,
-      {appleCount: 1 + data.data.appleCount}
+      {appleCount: ((data.data && data.data.appleCount)? data.data.appleCount +1 : 1)}
       //[{ "op": "add", "path": "/appleCount", "value": 1}]
     ); 
     return res.status(200).end();
